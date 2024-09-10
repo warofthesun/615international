0a7ebc2a458f187bcb23d44c0635bae073e471d5
Dan Brown <fireflip@gmail.com>
2024-09-10 12:08:59 -0500

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+// Bones Scripts File
+
+jQuery(document).ready(function($) {
+	$("#mobile-nav").click(function(){
+    $(".nav").toggleClass("show");
+		$("#mobile-nav").toggleClass("show");
+	});
+});
